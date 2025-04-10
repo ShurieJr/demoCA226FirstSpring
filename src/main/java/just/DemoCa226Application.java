07f940bc91b4441bb3077b62c6d7b44769db16c7
Hp <shuuriyejr@gmail.com>
2025-04-10 14:46:06 +0300

    Initial commit

---
+++ src/main/java/just/DemoCa226Application.java
@@ -0,0 +1,13 @@
+package just;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCa226Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCa226Application.class, args);
+	}
+
+}
---
+++ src/test/java/just/DemoCa226ApplicationTests.java
@@ -0,0 +1,13 @@
+package just;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCa226ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
